b489bfa6b2f4dca67b270564f85dbf7321a1b812
Cesar Roman <ing.cesaroman@gmail.com>
2025-12-17 14:11:20 -0600

    feat: Add multi-language support (ES, DE) and language switcher\n\n- Added Spanish and German versions of the CV data and components.\n- Implemented routing for /es and /de paths using react-router-dom.\n- Created a HeaderMenu component for easy language switching.\n- Integrated the HeaderMenu into all language versions of the application.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import App from './App.tsx';
+import App_es from './App_es.tsx';
+import App_de from './App_de.tsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/es" element={<App_es />} />
+        <Route path="/de" element={<App_de />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
